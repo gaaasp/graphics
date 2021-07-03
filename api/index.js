e23e56244dcfbd989dfb2db93ec95c7025e3a8c2
Gasplas <gaspard.lasorne@protonmail.com>
2021-07-03 20:31:49 +0200

    Add Redis

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 import { cards } from "../config";
 import { getGraphicCards } from "../utils";
+import Redis from "ioredis";
 
 module.exports = async (_req, res) => {
+	const redis = new Redis(process.env.REDIS_URL);
+	let oldProducts = await redis.get("products");
+	console.log(oldProducts);
+	oldProducts = oldProducts ? JSON.parse(oldProducts) : [];
 	const products = await getGraphicCards();
-	res.json(
+	const wanted = (products) =>
 		products.filter(({ name, price }) =>
 			cards.find(
 				(card) =>
@@ -12,6 +17,23 @@ module.exports = async (_req, res) => {
 						.indexOf(card.name.toLocaleLowerCase()) >= 0 &&
 					price <= card.price
 			)
+		);
+	const removed = wanted(
+		oldProducts.filter(
+			({ name, site }) =>
+				!products.find(
+					(product) => product.name === name && product.site === site
+				)
 		)
 	);
+	const added = wanted(
+		products.filter(
+			({ name, site }) =>
+				!oldProducts.find(
+					(product) => product.name === name && product.site === site
+				)
+		)
+	);
+	await redis.set("products", JSON.stringify(products));
+	res.json({ added, removed });
 };
