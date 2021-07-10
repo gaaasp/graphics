c9d92fdfb45dc82e53a07f64e4e2a896cba8dffe
Gasplas <gaspard.lasorne@protonmail.com>
2021-07-10 15:33:59 +0200

    Fix Cybertek

--- sites/cybertek.js
+++ sites/cybertek.js
@@ -1,60 +1,71 @@
 import { pages } from "../utils/index.js";
 
 export default async function getGraphicCards() {
-  return await pages(
-    (doc) => [
-      doc
-        ? Array.from(
+    return await pages(
+        (doc) => [
+            doc &&
             doc.querySelector(
-              "#content_product > div > div.categorie-r.categorie-block-r > div.categorie-filtre.lst_grid"
-            ).children
-          )
-            .filter(
-              (product) =>
-                Array.from(
-                  product.children[2].children[0].children[0].children
-                ).filter((a) => a.nodeName === "DIV")[1].children[0]
-                  .children[0].textContent === "En Stock" ||
-                Array.from(
-                  product.children[2].children[0].children[0].children
-                ).filter((a) => a.nodeName === "DIV")[1].children[0]
-                  .children[0].textContent === "Dernière pièce"
+                "#content_product > div > div.categorie-r.categorie-block-r > div.categorie-filtre.lst_grid"
             )
-            .map((product) => ({
-              name: Array.from(
-                product.children[2].children[0].children[0].children
-              )
-                .filter((p) => p.nodeName === "DIV")[0]
-                .children[0].textContent.split("- ")[0]
-                .trim(),
-              price: parseFloat(
-                product.children[3].children[0].children[0].textContent.replace(
-                  "€",
-                  "."
-                )
-              ),
-              url: product.children[0].href,
-              site: "cybertek",
-            }))
-        : [],
-      doc
-        ? Math.ceil(
-            parseFloat(
-              doc
-                .querySelector("#_ctl0_ContentPlaceHolder1_nb_result > div")
-                .textContent.replace(/ /g, "")
-            ) /
-              Array.from(
-                doc.querySelector(
-                  "#content_product > div > div.categorie-r.categorie-block-r > div.categorie-filtre.lst_grid"
-                ).children
-              ).length
-          )
-        : 1,
-    ],
-    {
-      base: "cybertek.fr/carte-graphique-6.aspx",
-      page: (i) => `cybertek.fr/carte-graphique-6.aspx?page=${i}`,
-    }
-  );
+                ? Array.from(
+                      doc.querySelector(
+                          "#content_product > div > div.categorie-r.categorie-block-r > div.categorie-filtre.lst_grid"
+                      ).children
+                  )
+                      .filter(
+                          (product) =>
+                              Array.from(
+                                  product.children[2].children[0].children[0]
+                                      .children
+                              ).filter((a) => a.nodeName === "DIV")[1]
+                                  .children[0].children[0].textContent ===
+                                  "En Stock" ||
+                              Array.from(
+                                  product.children[2].children[0].children[0]
+                                      .children
+                              ).filter((a) => a.nodeName === "DIV")[1]
+                                  .children[0].children[0].textContent ===
+                                  "Dernière pièce"
+                      )
+                      .map((product) => ({
+                          name: Array.from(
+                              product.children[2].children[0].children[0]
+                                  .children
+                          )
+                              .filter((p) => p.nodeName === "DIV")[0]
+                              .children[0].textContent.split("- ")[0]
+                              .trim(),
+                          price: parseFloat(
+                              product.children[3].children[0].children[0].textContent.replace(
+                                  "€",
+                                  "."
+                              )
+                          ),
+                          url: product.children[0].href,
+                          site: "cybertek",
+                      }))
+                : [],
+            doc?.querySelector("#_ctl0_ContentPlaceHolder1_nb_result > div")
+                ?.textContent
+                ? Math.ceil(
+                      parseFloat(
+                          doc
+                              .querySelector(
+                                  "#_ctl0_ContentPlaceHolder1_nb_result > div"
+                              )
+                              .textContent.replace(/ /g, "")
+                      ) /
+                          Array.from(
+                              doc.querySelector(
+                                  "#content_product > div > div.categorie-r.categorie-block-r > div.categorie-filtre.lst_grid"
+                              ).children
+                          ).length
+                  )
+                : 1,
+        ],
+        {
+            base: "cybertek.fr/carte-graphique-6.aspx",
+            page: (i) => `cybertek.fr/carte-graphique-6.aspx?page=${i}`,
+        }
+    );
 }
